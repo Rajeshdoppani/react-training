ebba887c918f6ca95415361eba36205665a6c5f9
Rajeshdoppani <47586417+Rajeshdoppani@users.noreply.github.com>
2022-12-17 18:47:01 +0530

    1 file modified, 2 added

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ExpenseItems.css';
 
+import ExpenseDate from './ExpenseDate';
+
 function ExpenseItems(props) {
   return (
     <div className="expense-item">
-      <div className="expense-item__date">{props.date.toLocaleString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
