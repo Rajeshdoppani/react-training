a8785ffd452eb681e3876c1a4828e2693413c717
Rajeshdoppani <47586417+Rajeshdoppani@users.noreply.github.com>
2022-12-17 16:29:03 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.css';
 
+import ExpenseItems from './components/ExpenseItems';
+
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <p>Hello</p>
+      <ExpenseItems></ExpenseItems>
     </div>
   );
 }
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ExpenseItems() {
+  return (
+    <div>
+      <h2>May 07 2022</h2>
+      <div>
+        <h2>Books</h2>
+        <p>$89.98</p>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItems;
